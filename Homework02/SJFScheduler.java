b22bf5eeb9caa6d189b2c4eebd204bb2b28a1b73
Patbu13 <70173620+Patbu13@users.noreply.github.com>
2022-07-14 19:11:24 -0400

    feel like im close to SRTF

--- Homework02/SJFScheduler.java
+++ Homework02/SJFScheduler.java
@@ -1,7 +1,35 @@
+/*
+ * Written by Patrick Burroughs - CSCE 311
+ * JJ Shepherd - 7/14/22
+ * Shortest Job First Scheduler
+ */
+
 import java.util.*;
+
 public class SJFScheduler extends BasicScheduler {
     
     public SJFScheduler() {
         this.readyQ = new PriorityQueue<BasicPCB>();
     }
+
+    /**
+	 * Selects the next process to become the running process
+	 */
+	public void dispatch()
+	{
+		if(readyQ.isEmpty())
+		{
+			runningProcess = null;
+			return;
+		}
+        
+		runningProcess = readyQ.poll();
+	}
+
+    public void addProcess(BasicPCB p)
+	{
+		totalProcesses++;
+        p.setPriority(p.getTotalLines());
+		readyQ.add(p);
+	}
 }
--- Homework02/SRTFScheduler.java
+++ Homework02/SRTFScheduler.java
@@ -1,3 +1,60 @@
+/*
+ * Written by Patrick Burroughs - CSCE 311
+ * JJ Shepherd - 7/14/22
+ * Shortest Remaining Time First Scheduler
+ */
+import java.util.*;
+
 public class SRTFScheduler extends BasicScheduler {
-    
+    private int localCount; // Notifies updateRunningProcess when process is added
+    public SRTFScheduler() {
+        this.readyQ = new PriorityQueue<BasicPCB>();
+    }
+
+    /**
+	 * Selects the next process to become the running process
+	 */
+	public void dispatch()
+	{
+		if(readyQ.isEmpty())
+		{
+			runningProcess = null;
+			return;
+		}        
+        
+		runningProcess = readyQ.poll();
+	}
+
+    public void updateRunningProcess()
+	{
+		if(runningProcess == null)
+		{
+			dispatch();
+			return;
+		}
+		runningProcess.nextLine();
+		if(runningProcess.hasCompleted())
+		{
+			runningProcess.setCompletionTick(tickCount);
+			waitingTimeSum += (runningProcess.getCompletionTick() - runningProcess.getArrivalTick());
+			dispatch();
+		}
+        else {
+            if (readyQ.peek() != runningProcess && localCount < totalProcesses) {
+                
+                addProcess(getRunningProcess());
+                localCount = totalProcesses + 1;
+                dispatch();
+                
+            }
+        }
+	}
+
+    public void addProcess(BasicPCB p)
+	{
+		totalProcesses++;
+        p.setPriority(p.getTotalLines()-p.getProgramCounter());
+		readyQ.add(p);
+	}
+
 }
