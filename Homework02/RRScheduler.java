3b75956824c28e712eea0a5531b1c4ddbd4adb61
Patbu13 <patrickbu_SC@hotmail.com>
2022-07-15 19:44:03 -0400

    started RR, think i fixed laptop

--- Homework02/RRScheduler.java
+++ Homework02/RRScheduler.java
@@ -1,8 +1,44 @@
 import java.util.*;
 public class RRScheduler extends BasicScheduler {
-    
+
+    private int quantum; // Sets quantuminterval for the scheduler
+    private int currentPTick; // Keeps track of ticks the current process has had consecutively
+
     public RRScheduler(int RR) {
-        this.readyQ = new PriorityQueue<BasicPCB>();
-        
+        this.readyQ = new LinkedList<BasicPCB>();
+        quantum = RR;
     }
+
+    public void dispatch()
+    {
+		if(readyQ.isEmpty())
+		{
+			runningProcess = null;
+			return;
+		}        
+        
+		runningProcess = readyQ.poll();
+    }
+
+    public void updateRunningProcess()
+	{
+        currentPTick++;
+		if(runningProcess == null)
+		{
+            currentPTick = 0;
+			dispatch();
+			return;
+		}
+		runningProcess.nextLine();
+		if(runningProcess.hasCompleted())
+		{
+			runningProcess.setCompletionTick(tickCount);
+			waitingTimeSum += (runningProcess.getCompletionTick() - runningProcess.getArrivalTick());
+			dispatch();
+		}   else if (currentPTick == quantum) {
+            addProcess(runningProcess);
+            dispatch();
+        }
+	}
+
 }
