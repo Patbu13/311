6766dc93fc1f03b68b091c909923be9e90397e75
Patbu13 <patrickbu_SC@hotmail.com>
2022-07-17 21:02:27 -0400

    Commmitting? Not sure what.

--- Homework02/RRScheduler.java
+++ Homework02/RRScheduler.java
@@ -7,8 +7,7 @@ import java.util.*;
 public class RRScheduler extends BasicScheduler {
 
     private int quantum; // Sets quantuminterval for the scheduler
-    private int currentPTick; // Keeps track of ticks the current process has had consecutively
-
+    
     public RRScheduler(int RR) {
         this.readyQ = new LinkedList<BasicPCB>();
         quantum = RR; //used to keep track of the quantum number for this instance
@@ -33,23 +32,19 @@ public class RRScheduler extends BasicScheduler {
 	 */
     public void updateRunningProcess()
 	{
-        currentPTick++;
 		if(runningProcess == null)
 		{
-            currentPTick = 0;
 			dispatch();
 			return;
 		}
 		runningProcess.nextLine();
 		if(runningProcess.hasCompleted())
 		{
-			currentPTick = 0;
 			runningProcess.setCompletionTick(tickCount);
 			waitingTimeSum += (runningProcess.getCompletionTick() - runningProcess.getArrivalTick());
 			dispatch();
-		}   else if (currentPTick == quantum) {
-            addProcess(runningProcess);
-			currentPTick = 0;
+		}   else if (runningProcess.getProgramCounter() % quantum == 0) {
+			addProcess(runningProcess);
             dispatch();
         }
 	}
